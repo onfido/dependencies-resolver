108b543b9ed8cea8626368b9e9e72d17c279b1e3
Avihoo Mamka <avihoo.mamka@gmail.com>
2017-04-07 16:44:49 +0100

    Bump version due to error

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requirements = [str(ir.req) for ir in install_requirements]
 
 setup(
     name='dependencies-resolver',
-    version='0.0.6',
+    version='0.0.9',
     description='Command line tool for downloading dependencies from config '
                 'file',
     long_description=readme,
